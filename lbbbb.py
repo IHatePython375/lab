d604ce6005c6e6aab436e76f55ad2e6b7396e3b4
jasonhe <jhe2@ufl.edu>
2024-04-11 13:21:14 -0400

    decode

--- lbbbb.py
+++ lbbbb.py
@@ -16,3 +16,9 @@ def encode(n):
 
     return str_out
 
+def decode(passcode, newpass):
+    for i in range(0, len(passcode)):
+        newpass += str(int(passcode[i]) - 3)
+        print("The encoded password is " + passcode + ", and the original password is " + newpass + ".")
+#hello
+
