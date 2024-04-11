194c4ae5b10573c2b80a89fc9dfb34723c242ea0
IHatePython375 <nguyendinhhung375@gmail.com>
2024-04-11 12:55:39 -0400

    commited file

---
+++ lbbbb.py
@@ -0,0 +1,20 @@
+def encode(n):
+    my_list = []
+    new_list = []
+    str_out = ''
+    for i in n:
+        my_list.append(int(i))
+
+    for num in my_list:
+        if num < 7:
+            new_list.append(num+3)
+        else:
+            new_list.append((num+3)%10)
+
+    for num in new_list:
+        str_out += str(num)
+
+    return str_out
+
+
+print(encode('00009962'))
